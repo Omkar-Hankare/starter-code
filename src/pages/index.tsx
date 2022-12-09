48538492e3437cc63959369f22e294e816bac583
Omkar Hankare <omkarhankare1258@gmail.com>
2022-12-09 15:21:13 +0530

    chore: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Trading Journal</title>
+        <meta name="Trading Journal" content="" />
+        <link rel="icon" href="" />
+      </Head>
+
+      <main>main</main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
